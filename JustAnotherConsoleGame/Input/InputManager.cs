8f35885b61b37d677a7b4e6f462fb0538c162da8
Daniel Henk <henk.daniel98@gmail.com>
2018-10-31 00:26:25 +0100

    inputmanager

---
+++ JustAnotherConsoleGame/Input/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JustAnotherConsoleGame.Input
+{
+    public class InputManager
+    {
+        public bool IsRunning { get; private set; }
+
+        private Dictionary<ConsoleKey, List<Action>> bindings = new Dictionary<ConsoleKey, List<Action>>();
+
+        public void AddBinding(ConsoleKey key, Action action)
+        {
+            this.bindings.TryGetValue(key, out List<Action> actions);
+
+            if (actions == null)
+            {
+                actions = new List<Action>();
+                actions.Add(action);
+                this.bindings.Add(key, actions);
+            }
+            else
+            {
+                actions.Add(action);
+                this.bindings[key] = actions;
+            }
+        }
+
+        public InputManager Bind(Action action, params ConsoleKey[] keys)
+        {
+            foreach (var key in keys)
+                this.AddBinding(key, action);
+
+            return this;
+        }
+
+        public void Run()
+        {
+            this.IsRunning = true;
+
+            while (this.IsRunning)
+                if (this.bindings.TryGetValue(Console.ReadKey(true).Key, out List<Action> actions))
+                    actions.ForEach(x => x.Invoke());
+        }
+
+        public void Stop()
+        {
+            this.IsRunning = false;
+        }
+    }
+}
--- JustAnotherConsoleGame/Program.cs
+++ JustAnotherConsoleGame/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using JustAnotherConsoleGame.Input;
+using System;
+using System.Text;
 
 namespace JustAnotherConsoleGame
 {
     class Program
     {
+        private static InputManager inputManager = new InputManager();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += (s, e) => Environment.Exit(0);
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+
+            inputManager
+                .Bind(MooveLeft, ConsoleKey.LeftArrow, ConsoleKey.A)
+                .Bind(MooveRight, ConsoleKey.RightArrow, ConsoleKey.D)
+                .Bind(MooveUp, ConsoleKey.UpArrow, ConsoleKey.W)
+                .Bind(MooveDown, ConsoleKey.DownArrow, ConsoleKey.S);
+
+
+            inputManager.Run();
+            Console.WriteLine("stopped");
+            Console.ReadLine();
+        }
+
+        static void MooveLeft()
+        {
+            Console.Title = "left";
+        }
+
+        static void MooveRight()
+        {
+            Console.Title = "right";
+        }
+
+        static void MooveUp()
+        {
+            Console.Title = "up";
+        }
+
+        static void MooveDown()
+        {
+            Console.Title = "down";
         }
     }
 }
